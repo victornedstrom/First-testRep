a8654f4ea34da4ee5685d777bb1f639f39b6470e
victornedstrom <vicotr.nedstrom@gmail.com>
2013-02-25 18:30:31 +0100

    testigen :P lol :P

--- testProject/src/testklassosv.java
+++ testProject/src/testklassosv.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 public class testklassosv {
 	
@@ -9,6 +11,17 @@ public class testklassosv {
 		//Detta är ett testprojekt
 		int c = 4; 
 		//lol
+		System.out.println(c);
+		Random rand = new Random();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i<10;i++){
+			
+			double temp = rand.nextDouble();
+			list.add((int) temp);
+			System.out.println(i+1 + ": " + temp);
+			
+		}
+		
 		
 	}
 	
